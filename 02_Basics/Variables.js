737b8db5a0b9ff7248e1d86f734a40e2c4126e23
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-06-26 18:51:42 +0530

    Variables Second

---
+++ 02_Basics/Variables.js
@@ -0,0 +1,13 @@
+var fullName = "Dev";
+var isLoggedIn = false;
+var loggedCount = 34;
+console.log(loggedCount);
+// This will print 34
+console.log("loggedCount");
+// This will print loggedCount
+var paymentMode;
+console.log(paymentMode);
+// This will print Undefined
+paymentMode = "Online";
+console.log(paymentMode);
+// This will Print Online
---
+++ 02_Basics/Variables2.js
@@ -0,0 +1,10 @@
+var fullName="Deo Raj"
+const uid="20BCS9822";
+var email="d.jrajsingh81@gmail.com"
+var password="123456"
+var confirmPassword="123456"
+console.log(`
+With Unique ID: ${uid}
+User's Name is: ${fullName}
+and his email is:${email}
+`)

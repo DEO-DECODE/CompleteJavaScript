3685639609e9fdc04f9c97b031f88959b9d4a0e9
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-06-27 23:55:07 +0530

    Data types in JS

---
+++ 02_Basics/Datatypes.js
@@ -0,0 +1,19 @@
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+console.log(typeof 42);
+let a;
+let obj = {
+  x: "Dev",
+  y: "Shruti",
+  uid: 9822,
+};
+console.log(typeof obj)
+console.log(a);
+console.log(obj);

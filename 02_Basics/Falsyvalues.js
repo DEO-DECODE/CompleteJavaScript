edd3e5c73c9130bf963737993b575850ccb519d7
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-06-28 00:45:34 +0530

    Falsy values.js

---
+++ 02_Basics/Falsyvalues.js
@@ -0,0 +1,17 @@
+/*
+Falsy values are values that resolve to the Boolean false.
+In JavaScript, we have 6 falsy values:
+false
+0 (zero)
+‘’ or “” (empty string)
+null
+undefined
+NaN
+All these return false when they are evaluated.
+*/
+// var user = "";
+// user ? console.log("Condition is true.") : console.log("Condition is false");
+// var user = null;
+// user ? console.log("Condition is true.") : console.log("Condition is false");
+var user ;
+user ? console.log("Condition is true.") : console.log("Condition is false");

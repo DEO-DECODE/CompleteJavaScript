6d45a542143dd175dbea131033d73b6edf03ba11
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-06-28 00:01:37 +0530

    Conditionals in JS

---
+++ 02_Basics/Conditionals.js
@@ -0,0 +1,10 @@
+let temp=34;
+if(temp<20){
+    console.log("It's very cold outsid");
+}
+else if(temp<32){
+    console.log("It's moderate outside");
+}
+else{
+    console.log("It's Hot outside");
+}

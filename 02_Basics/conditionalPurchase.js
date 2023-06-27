dba6adf8252668a3aa3fe962a133ec7406830207
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-06-28 00:08:43 +0530

    conditional Purchase

---
+++ 02_Basics/conditionalPurchase.js
@@ -0,0 +1,9 @@
+let google = false;
+let facebook = false;
+let instagram = false;
+if(google || facebook || instagram){
+    console.log("Log in Sucess !!!!");
+}
+else{
+    console.log("Wrong Credentials !!!!");
+}

cdb517186734ac9410391621794601d621dfca3e
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-06-28 00:34:41 +0530

    Switch Case in JS

---
+++ 02_Basics/Switchcase.js
@@ -0,0 +1,27 @@
+// admin : gets full acess
+// subadmin : gets acess to create and delete courses
+// testprep : gets acess to create and delete courses.
+// user : gets access to consume content.
+var user = "adm";
+switch (user) {
+  case "admin": {
+    console.log("gets full acess");
+    break;
+  }
+  case "subadmin": {
+    console.log("gets acess to create and delete courses");
+    break;
+  }
+  case "testprep": {
+    console.log("gets acess to create and delete courses.");
+    break;
+  }
+  case "user": {
+    console.log("gets access to consume content");
+    break;
+  }
+  default: {
+    console.log("Trial User");
+    break;
+  }
+}

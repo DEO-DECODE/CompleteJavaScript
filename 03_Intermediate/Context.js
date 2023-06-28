646107fa63a914c87b6003df8688c002f73d93c1
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-06-29 01:14:17 +0530

    Context in JavaScript

---
+++ 03_Intermediate/Context.js
@@ -0,0 +1,6 @@
+/*
+Context is always the value of the this keyword which is a reference to the object that “owns” the currently executing code or the function where it’s looked at.
+windowis a global object in the browser so if we type this in the console and it should return window object, which it does.
+Let us say foo is a function.
+calling foo and window.foo is same.
+*/

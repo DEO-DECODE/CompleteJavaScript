eae4df40eae60d44e3d3403cd7e388274fdc97ff
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-07-01 00:30:36 +0530

    Objects in JavaScript

---
+++ 03_Intermediate/objects.js
@@ -0,0 +1,10 @@
+var users = {
+  firstName: "Dev",
+  lastName: "Raj",
+  role: "Admin",
+  loginCount: 37,
+  facebookSignedIn: true,
+};
+users.loginCount=45;
+// console.log(users);
+console.table(users);

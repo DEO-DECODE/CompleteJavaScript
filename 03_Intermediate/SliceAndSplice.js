a9d6118b4e68d7bf0747f7cb4e5c0fa4c7268fd1
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-07-01 00:20:42 +0530

    Slice and Splice in JS

---
+++ 03_Intermediate/SliceAndSplice.js
@@ -0,0 +1,6 @@
+var users=["Tim", "Ted", "Ton", "Sam", "Sor", "Sod"];
+// console.log(users.slice(1,5));
+// users.splice(1, 3, "Dev");
+// users.splice(1, 3, "Dev", "Raj");
+// users.splice(1, 3, "Dev", "Raj", "Bhardwaj");
+console.log(users);

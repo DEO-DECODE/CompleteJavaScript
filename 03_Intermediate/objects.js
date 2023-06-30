fef057020f6ddd6d0de6bf5b478eda14fb545dbc
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-07-01 00:42:10 +0530

    objects final js

--- 03_Intermediate/objects.js
+++ 03_Intermediate/objects.js
@@ -4,7 +4,23 @@ var users = {
   role: "Admin",
   loginCount: 37,
   facebookSignedIn: true,
+  courseList: [],
+  byCourse: function (courseName) {
+    this.courseList.push(courseName);
+    /*
+     This will be simply used to refer to the courseList (array) of users object
+     */
+  },
+  getCourseCount: function () {
+    return `${this.firstName} is enrolled in total of ${this.courseList.length} courses`;
+  },
 };
-users.loginCount=45;
+// users.loginCount = 45;
 // console.log(users);
-console.table(users);
+// console.table(users);
+var courseList=true;
+console.log(users.firstName);
+console.log(users.getCourseCount());
+users.byCourse("React JS Course");
+users.byCourse("Angular JS");
+console.log(users.getCourseCount());

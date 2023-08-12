3991cf6601c22d9b60953cf93e9c91274ddf2702
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-08-12 19:56:10 +0530

    Async Await Revisited

--- Async_JS/PromiseByH.js
+++ Async_JS/PromiseByH.js
@@ -1,32 +1,41 @@
 /*
 The Promise object represents the eventual completion or failure of an asynchronous operation and it's resulting value
+The Promise() constructor takes a function as an argument. The function also accepts two functions resolve() and reject().
+
+If the promise returns successfully, the resolve() function is called. And, if an error occurs, the reject() function is called.
 */
+console.log(1);
 let p1 = new Promise((resolve, reject) => {
   console.log("Promise is pending");
   setTimeout(() => {
-    console.log("I am a promise and I am resolved")
+    console.log("I am a promise and I am resolved");
     resolve(true);
-  }, 5000);
+  }, 2000);
 });
-
+console.log(2);
 let p2 = new Promise((resolve, reject) => {
   console.log("Promise is pending");
   setTimeout(() => {
-    console.log("I am a promise and I am rejected")
+    console.log("I am a promise and I am rejected");
     reject(new Error("I am an error"));
-  }, 5000);
+  }, 4000);
 });
-
+console.log(3);
 // To get the value
-p1.then((value) => {
-  console.log(value);
-});
+p1.then(
+  (value) => {
+    console.log(value);
+  },
+  (error) => {
+    console.log(error);
+  }
+);
 
 // To catch the errors
 // p2.catch((error) => {
 //   console.log("some error occurred in p2"+ error);
 // });
-
+console.log(4);
 p2.then(
   (value) => {
     console.log(value);
@@ -35,3 +44,4 @@ p2.then(
     console.log(error);
   }
 );
+console.log(5);
---
+++ Async_JS/PromiseChainingByH.js
@@ -0,0 +1,42 @@
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Resolved after 2 seconds");
+    resolve(56);
+  }, 2000);
+});
+
+p1.then(
+  (value) => {
+    console.log(value);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Promise 2");
+      }, 2000);
+    });
+  },
+  (error) => {
+    console.log(error);
+  }
+)
+  .then(
+    (value) => {
+        console.log(value);
+      console.log("We are done");
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          reject("Unfortunately an error occured!");
+        });
+      }, 3000);
+    },
+    (error) => {
+      console.log(error);
+    }
+  )
+  .then(
+    (value) => {
+      console.log("Now we are pakka done");
+    },
+    (error) => {
+      console.log(error);
+    }
+  );

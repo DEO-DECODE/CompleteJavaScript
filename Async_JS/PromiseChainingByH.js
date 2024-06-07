a8a8d6cf0671561ffe61b8adf6758098dadf7327
DEO RAJ <d.jrajsingh81@gmail.com>
2024-06-07 15:11:36 +0530

    Some Changes before new batch

--- Async_JS/PromiseChainingByH.js
+++ Async_JS/PromiseChainingByH.js
@@ -1,45 +1,46 @@
-// let p1 = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     console.log("Resolved after 2 seconds");
-//     reject(new Error("error occured!"));
-//   }, 2000);
-// });
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Resolved after 2 seconds");
+    reject(new Error("error occured!"));
+  }, 2000);
+});
 
-// p1.then(
-//   (value) => {
-//     console.log(value);
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve("Promise 2");
-//       }, 2000);
-//     });
-//   },
-//   (error) => {
-//     console.log(error);
-//   }
-// )
-//   .then(
-//     (value) => {
-//       console.log(value);
-//       console.log("We are done");
-//       return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//           reject(new Error("Unfortunately an error occured!"));
-//         });
-//       }, 3000);
-//     },
-//     (error) => {
-//       console.log(error);
-//     }
-//   )
-//   .then(
-//     (value) => {
-//       console.log("Now we are pakka done");
-//     },
-//     (error) => {
-//       console.log(error);
-//     }
-//   );
+p1.then(
+  (value) => {
+    console.log(value);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Promise 2");
+      }, 2000);
+    });
+  },
+  (error) => {
+    console.log(error);
+  }
+)
+  .then(
+    (value) => {
+      console.log(value);
+      console.log("We are done");
+      
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          reject(new Error("Unfortunately an error occured!"));
+        });
+      }, 3000);
+    },
+    (error) => {
+      console.log(error);
+    }
+  )
+  .then(
+    (value) => {
+      console.log("Now we are pakka done");
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 function fetchData() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
